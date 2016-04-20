fe28e35b30158781b21c6155809e83e629555cea
Alex Soto <asotobueno@cloudbees.com>
2016-04-20 17:16:01 +0200

    adds parallel

---
+++ src/test/java/org/superbiz/FooBarTest.java
@@ -0,0 +1,14 @@
+package org.superbiz;
+
+import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class FooBarTest {
+
+    @Test
+    public void thirdTest() throws InterruptedException {
+        TimeUnit.SECONDS.sleep(20);
+    }
+
+}
